50d3e0f6bfa1139f93e6c18532c0226bab779b3e
Gilbert Choy <gilbertchoy@gmail.com>
2018-07-17 13:52:29 -0700

    fragmentmanager works

--- app/src/main/java/com/me/gc/scratcher1/MainActivity.java
+++ app/src/main/java/com/me/gc/scratcher1/MainActivity.java
@@ -5,6 +5,7 @@ package com.me.gc.scratcher1;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -13,6 +14,8 @@ import android.util.Log;
 public class MainActivity extends FragmentActivity {
     private MainViewModel model;
     private Context context;
+    private FragmentManager fragmentManager;
+    private BottomFragment bottomFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,8 @@ public class MainActivity extends FragmentActivity {
         model.getSelected().observe(this, Integer -> {
             Log.d("berttest","item selected via main activity") ;
             // Create a new FragmentManager
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            this.fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
 
             // Create a new Fragment to be placed in the activity layout
             ScratcherCardFragment scratcherCardFragment = new ScratcherCardFragment();
@@ -36,11 +39,41 @@ public class MainActivity extends FragmentActivity {
             scratcherCardFragment.setArguments(getIntent().getExtras());
 
             // Add the fragment to the 'fragment_container' FrameLayout
-            fragmentTransaction.add(R.id.fragment_bottom, scratcherCardFragment);
-            fragmentTransaction.commit();
+            //fragmentTransaction.replace(R.id.fragment_bottom, )
+            //fragmentTransaction.add(R.id.fragment_bottom, scratcherCardFragment);
+            fragmentTransaction.replace(R.id.fragment_bottom, scratcherCardFragment)
+                    .addToBackStack(null).commit();
         });
 
+        if (findViewById(R.id.fragment_top) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            this.fragmentManager = getSupportFragmentManager();
+            // Create a new Fragment to be placed in the activity layout
+            FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
+            TopFragment topFragment = new TopFragment();
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            topFragment.setArguments(getIntent().getExtras());
+            // Add the fragment to the 'fragment_container' FrameLayout
+            fragmentTransaction.add(R.id.fragment_top, topFragment);
+            fragmentTransaction.commit();
+        }
 
+        if (findViewById(R.id.fragment_bottom) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            this.fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
+            this.bottomFragment = new BottomFragment();
+            this.bottomFragment.setArguments(getIntent().getExtras());
+            fragmentTransaction.add(R.id.fragment_bottom, this.bottomFragment);
+            fragmentTransaction.commit();
+        }
+
+        /*
         if (findViewById(R.id.fragment_top) != null) {
 
             // However, if we're being restored from a previous state,
@@ -92,5 +125,6 @@ public class MainActivity extends FragmentActivity {
             fragmentTransaction.add(R.id.fragment_bottom, bottomFragment);
             fragmentTransaction.commit();
         }
+        */
     }
 }
