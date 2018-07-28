551073cdf9ea47ea49fd6d6ab63ea434998ccc3d
Gilbert Choy <gilbertchoy@gmail.com>
2018-07-27 18:27:11 -0700

    added main scratcher

--- app/src/main/java/com/me/gc/scratcher1/ScratcherCardFragment.java
+++ app/src/main/java/com/me/gc/scratcher1/ScratcherCardFragment.java
@@ -1,6 +1,7 @@
 package com.me.gc.scratcher1;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +11,18 @@ import android.widget.RelativeLayout;
 
 public class ScratcherCardFragment extends Fragment {
     private ImageView backgroundImage;
+    private ImageView scorecardBackgroundImage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.scratcher_card, container, false);
         backgroundImage = v.findViewById(R.id.backgroundImage);
-
         backgroundImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
         //backgroundImage.setBackground(getActivity().getDrawable(R.drawable.test));
         backgroundImage.setImageResource(R.drawable.scratcher1);
+        //scorecardBackgroundImage = v.findViewById(R.id.scorecardBackgroundImage);
+        //scorecardBackgroundImage.setImageResource(R.drawable.greypattern);
 
         return v;
     }
