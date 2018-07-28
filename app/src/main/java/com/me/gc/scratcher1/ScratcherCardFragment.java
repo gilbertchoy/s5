939841d08a38a96b2fae5b3b56f66b4daa928be5
Gilbert Choy <gilbertchoy@gmail.com>
2018-07-28 01:49:24 -0700

    create point system

--- app/src/main/java/com/me/gc/scratcher1/ScratcherCardFragment.java
+++ app/src/main/java/com/me/gc/scratcher1/ScratcherCardFragment.java
@@ -1,17 +1,24 @@
 package com.me.gc.scratcher1;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import com.me.gc.scratcher1.ScratchImageView;
 
 public class ScratcherCardFragment extends Fragment {
     private ImageView backgroundImage;
     private ImageView scorecardBackgroundImage;
+    private com.me.gc.scratcher1.ScratchImageView scratchImageView;
+    private ScratchImageView extraScratchImageView;
+    private MainViewModel viewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,11 +26,47 @@ public class ScratcherCardFragment extends Fragment {
         View v = inflater.inflate(R.layout.scratcher_card, container, false);
         backgroundImage = v.findViewById(R.id.backgroundImage);
         backgroundImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        //backgroundImage.setBackground(getActivity().getDrawable(R.drawable.test));
         backgroundImage.setImageResource(R.drawable.scratcher1);
-        //scorecardBackgroundImage = v.findViewById(R.id.scorecardBackgroundImage);
-        //scorecardBackgroundImage.setImageResource(R.drawable.greypattern);
+
+        //selected value
+        viewModel = ViewModelProviders.of(this.getActivity()).get(MainViewModel.class);
+        int selected = (int) viewModel.getSelected().getValue();
+
+        //set values for loot
+
+
+
+
+
+        scratchImageView = (ScratchImageView) v.findViewById(R.id.scratchImage);
+
+        scratchImageView.setRevealListener(new ScratchImageView.IRevealListener() {
+            @Override
+            public void onRevealed(ScratchImageView tv) {
+                Log.d("berttest", "onReveal");
+            }
+
+            @Override
+            public void onRevealPercentChangedListener(ScratchImageView siv, float percent) {
+                Log.d("berttest", "on percent change: " + percent);
+            }
+        });
+
+        extraScratchImageView = (ScratchImageView) v.findViewById(R.id.extraScratchImage);
+        extraScratchImageView.setRevealListener(new ScratchImageView.IRevealListener() {
+            @Override
+            public void onRevealed(ScratchImageView tv) {
+                // on reveal
+            }
+
+            @Override
+            public void onRevealPercentChangedListener(ScratchImageView siv, float percent) {
+                Log.d("berttest", "percent change1: " + percent);
+            }
+        });
 
         return v;
     }
+
+
 }
