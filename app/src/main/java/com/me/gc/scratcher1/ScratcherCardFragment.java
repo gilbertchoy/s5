ab5b139aa10ec386e5df463ab8a443d2b8532609
Gilbert Choy <gilbertchoy@gmail.com>
2018-10-01 17:14:03 -0700

    more

--- app/src/main/java/com/me/gc/scratcher1/ScratcherCardFragment.java
+++ app/src/main/java/com/me/gc/scratcher1/ScratcherCardFragment.java
@@ -20,6 +20,7 @@ import com.airbnb.lottie.LottieAnimationView;
 import java.util.Random;
 
 import static android.view.View.VISIBLE;
+import static android.widget.ImageView.ScaleType.FIT_START;
 
 public class ScratcherCardFragment extends Fragment {
     private ImageView backgroundImage;
@@ -56,9 +57,13 @@ public class ScratcherCardFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.scratcher_card1, container, false);
         backgroundImage = v.findViewById(R.id.backgroundImage);
-        backgroundImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
         //backgroundImage.setImageResource(R.drawable.scratcher1);
-        backgroundImage.setImageResource(R.drawable.lg_drone1);
+        backgroundImage.setImageResource(R.drawable.lg_drone2);
+        backgroundImage.setScaleType(FIT_START);
+
+
+
+
 
         //revealFlag set to true in beginning and changes to false after both scratchers fields revealed
         revealFlag = true;
