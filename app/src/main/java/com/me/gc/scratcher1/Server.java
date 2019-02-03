ac65b8685d53a869f5f2ebda77c2539b6a43c09c
Gilbert Choy <gilbertchoy@gmail.com>
2019-02-02 23:17:56 -0800

    reverse

--- app/src/main/java/com/me/gc/scratcher1/Server.java
+++ app/src/main/java/com/me/gc/scratcher1/Server.java
@@ -135,6 +135,7 @@ public class Server {
             try {
                 hash = hashing.sha1(deviceuid + "." + hashkey + "." + deviceInfo.get("model"));
                 Log.d("berttest", "hash is" + hash);
+                Log.d("berttest", "model is: " + deviceInfo.get("model"));
             } catch (NoSuchAlgorithmException e) {
                 e.printStackTrace();
             } catch (UnsupportedEncodingException e) {
