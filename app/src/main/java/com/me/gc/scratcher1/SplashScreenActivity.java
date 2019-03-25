5cd06ae5a8d236732d81b1fbb7c7b96cfefd8998
Gilbert Choy <gilbertchoy@gmail.com>
2019-03-25 16:55:13 -0700

    working on splashscreen

---
+++ app/src/main/java/com/me/gc/scratcher1/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.me.gc.scratcher1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashcreenlayout);
+
+        Log.d("berttest","splashscreen activity");
+
+        Intent intent = new Intent(getApplicationContext(),
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
